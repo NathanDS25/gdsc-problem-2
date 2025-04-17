1cc9640a64bf7475d0883de72da081a4607f5393
NathanDS25 <89599599+NathanDS25@users.noreply.github.com>
2025-04-17 10:43:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const eventDate = new Date(); // Create a new Date object for the current date and time.
+eventDate.setDate(eventDate.getDate() + 10);  // Set the target date to 10 days from now.
+eventDate.setHours(10, 10, 10);  // Set the target time to 10:10:10 on that date.
